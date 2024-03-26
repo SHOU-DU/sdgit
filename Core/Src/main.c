dc14ebf9a0c22dd062631a54148b65fd727b1d9a
shou-du <769921010@qq.com>
2024-03-26 17:18:04 +0800

    sdgit init

---
+++ Core/Src/main.c
@@ -0,0 +1,322 @@
+/***
+	*************************************************************************************************
+	*	@file  	main.c
+	*	@version V1.0
+	*  @date    2021-8-4
+	*	@author  反客科技
+	*	@brief   SPI 驱动 W25Qxx系列相关函数
+   ************************************************************************************************
+   *  @description
+	*
+	*	实验平台：反客STM32H7B0VBT6核心板 （型号：FK7B0M1-VBT6）
+	*	淘宝地址：https://shop212360197.taobao.com
+	*	QQ交流群：536665479
+	*
+>>>>> 功能说明：
+	*
+	*	1.点亮LED，使用HAL库自带的延时函数实现闪烁
+	*	2.使用SPI6驱动W25Q64，进行简单的读写测试
+   *  3. SPI6 SCK驱动时钟配置为 50MHz
+	*
+>>>>> 串口打印说明：
+	*
+	*	使用 USART1 （PA9/PA10）打印输出信息，串口波特率115200
+	*	
+	************************************************************************************************
+***/
+
+#include "main.h"
+#include "led.h"
+#include "key.h"
+#include "usart.h"
+#include "spi_w25q64.h"
+
+/********************************************** 变量定义 *******************************************/
+
+#define W25Qxx_NumByteToTest   	32*1024				// 测试数据的长度，32K
+
+volatile int32_t  SPI_Status ; 		 //	检测标志位
+
+volatile  uint32_t W25Qxx_TestAddr  =	0	;				// 测试地址
+uint8_t  W25Qxx_WriteBuffer[W25Qxx_NumByteToTest];		//	写数据数组
+uint8_t  W25Qxx_ReadBuffer[W25Qxx_NumByteToTest];		//	读数据数组
+
+
+/***************************************************************************************************
+*	函 数 名: SPI_W25Qxx_Test
+*	入口参数: 无
+*	返 回 值: SPI_W25Qxx_OK - 测试成功并通过
+*	函数功能: 进行简单的读写测试，并计算速度
+*	说    明: 无	
+***************************************************************************************************/
+
+int8_t SPI_W25Qxx_Test(void)		//Flash读写测试
+{
+	uint32_t i = 0;	// 计数变量
+	uint32_t ExecutionTime_Begin;		// 开始时间
+	uint32_t ExecutionTime_End;		// 结束时间
+	uint32_t ExecutionTime;				// 执行时间	
+	float    ExecutionSpeed;			// 执行速度
+
+// 擦除 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   
+	
+	ExecutionTime_Begin 	= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	SPI_Status 			   = SPI_W25Qxx_BlockErase_32K(W25Qxx_TestAddr);	// 擦除32K字节
+	ExecutionTime_End		= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	
+	ExecutionTime = ExecutionTime_End - ExecutionTime_Begin; // 计算擦除时间，单位ms
+	
+	if( SPI_Status == SPI_W25Qxx_OK )
+	{
+		printf ("\r\nW25Q64 擦除成功, 擦除32K字节所需时间: %d ms\r\n",ExecutionTime);		
+	}
+	else
+	{
+		printf ("\r\n 擦除失败!!!!!  错误代码:%d\r\n",SPI_Status);
+		while (1);
+	}	
+
+// 写入 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+	for(i=0;i<W25Qxx_NumByteToTest;i++)  //先将数据写入数组
+	{
+		W25Qxx_WriteBuffer[i] = i;
+	}
+	ExecutionTime_Begin 	= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	SPI_Status				= SPI_W25Qxx_WriteBuffer(W25Qxx_WriteBuffer,W25Qxx_TestAddr,W25Qxx_NumByteToTest); // 写入数据
+	ExecutionTime_End		= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	
+	ExecutionTime  = ExecutionTime_End - ExecutionTime_Begin; 		// 计算擦除时间，单位ms
+	ExecutionSpeed = (float)W25Qxx_NumByteToTest / ExecutionTime ; // 计算写入速度，单位 KB/S
+	if( SPI_Status == SPI_W25Qxx_OK )
+	{
+		printf ("\r\n写入成功,数据大小：%d KB, 耗时: %d ms, 写入速度：%.2f KB/s\r\n",W25Qxx_NumByteToTest/1024,ExecutionTime,ExecutionSpeed);		
+	}
+	else
+	{
+		printf ("\r\n写入错误!!!!!  错误代码:%d\r\n",SPI_Status);
+		while (1);
+	}		
+
+// 读取	>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+	
+	ExecutionTime_Begin 	= HAL_GetTick();	// 获取 systick 当前时间，单位ms	
+	SPI_Status				= SPI_W25Qxx_ReadBuffer(W25Qxx_ReadBuffer,W25Qxx_TestAddr,W25Qxx_NumByteToTest);	// 读取数据
+	ExecutionTime_End		= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	
+	ExecutionTime  = ExecutionTime_End - ExecutionTime_Begin; 					// 计算擦除时间，单位ms
+	ExecutionSpeed = (float)W25Qxx_NumByteToTest/1024/1024 / ExecutionTime*1000 ; 	// 计算读取速度，单位 MB/S 
+	
+	if( SPI_Status == SPI_W25Qxx_OK )
+	{
+		printf ("\r\n读取成功,数据大小：%d KB, 耗时: %d ms, 读取速度：%.2f MB/s \r\n",W25Qxx_NumByteToTest/1024,ExecutionTime,ExecutionSpeed);		
+	}
+	else
+	{
+		printf ("\r\n读取错误!!!!!  错误代码:%d\r\n",SPI_Status);
+		while (1);
+	}	
+
+// 数据校验 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   
+
+	for(i=0;i<W25Qxx_NumByteToTest;i++)	//验证读出的数据是否等于写入的数据
+	{
+		if( W25Qxx_WriteBuffer[i] != W25Qxx_ReadBuffer[i] )	//如果数据不相等，则标明失败	
+		{
+			printf ("\r\n数据校验失败!!!!!出错数据位置:%d\r\n",i);	
+
+			while(1);
+		}
+	}		
+	printf ("\r\n校验通过!!!!! SPI驱动W25Q64测试正常\r\n");		
+	
+
+// 读取整片Flash的数据，用以测试速度 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	
+	printf ("\r\n*****************************************************************************************************\r\n");		
+	printf ("\r\n上面的测试中，读取的数据比较小，耗时很短，加之测量的最小单位为ms，计算出的读取速度误差较大\r\n");		
+	printf ("\r\n接下来读取整片flash的数据用以测试速度，这样得出的速度误差比较小\r\n");		
+	printf ("\r\n开始读取>>>>\r\n");		
+	ExecutionTime_Begin 	= HAL_GetTick();	// 获取 systick 当前时间，单位ms		
+	
+	for(i=0;i<SPI_W25Qxx_FlashSize/(W25Qxx_NumByteToTest);i++)	// 每次读取 W25Qxx_NumByteToTest 字节的数据
+	{
+		SPI_Status		 = SPI_W25Qxx_ReadBuffer(W25Qxx_ReadBuffer,W25Qxx_TestAddr,W25Qxx_NumByteToTest);
+		W25Qxx_TestAddr = W25Qxx_TestAddr + W25Qxx_NumByteToTest;		
+	}
+	ExecutionTime_End		= HAL_GetTick();	// 获取 systick 当前时间，单位ms
+	
+	ExecutionTime  = ExecutionTime_End - ExecutionTime_Begin; 								// 计算擦除时间，单位ms
+	ExecutionSpeed = (float)SPI_W25Qxx_FlashSize/1024/1024 / ExecutionTime*1000  ; 	// 计算读取速度，单位 MB/S 
+
+	if( SPI_Status == SPI_W25Qxx_OK )
+	{
+		printf ("\r\n读取成功,数据大小：%d MB, 耗时: %d ms, 读取速度：%.2f MB/s \r\n",SPI_W25Qxx_FlashSize/1024/1024,ExecutionTime,ExecutionSpeed);		
+	}
+	else
+	{
+		printf ("\r\n读取错误!!!!!  错误代码:%d\r\n",SPI_Status);
+		while (1);
+	}	
+	
+	return SPI_Status ;  	
+}
+
+/********************************************** 函数声明 *******************************************/
+
+void SystemClock_Config(void);
+
+/***************************************************************************************************
+*	函 数 名: main
+*	入口参数: 无
+*	返 回 值: 无
+*	函数功能: w25q64读写测试
+*	说    明: 无
+****************************************************************************************************/
+
+int main(void)
+{	
+	SCB_EnableICache();		// 使能ICache
+	SCB_EnableDCache();		// 使能DCache
+	HAL_Init();					// 初始化HAL库	
+	SystemClock_Config();	// 配置系统时钟，主频280MHz
+	LED_Init();					// 初始化LED引脚
+	KEY_Init();					// 初始化按键引脚
+	USART1_Init();				// USART1初始化	
+
+	SPI_W25Qxx_Init();      // W25Qxx 初始化
+
+	SPI_W25Qxx_Test();		//Flash读写测试
+
+	while (1)
+	{
+		LED1_Toggle;
+		HAL_Delay(100);
+	}
+}
+
+/****************************************************************************************************/
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 280000000 (CPU Clock)
+  *            HCLK(Hz)                       = 280000000 (AXI and AHBs Clock)
+  *            AHB Prescaler                  = 1 (AHB  Clock  280MHz)
+  *            D1 APB3 Prescaler              = 2 (APB3 Clock  140MHz)
+  *            D2 APB1 Prescaler              = 2 (APB1 Clock  140MHz)
+  *            D2 APB2 Prescaler              = 2 (APB2 Clock  140MHz)
+  *            D3 APB4 Prescaler              = 2 (APB4 Clock  140MHz)
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 5
+  *            PLL_N                          = 112
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 2
+  *            PLL_R                          = 2
+  *            VDD(V)                         = 3.3
+  *            Flash Latency(WS)              = 7
+  * @param  None
+  * @retval None
+  */
+/****************************************************************************************************/  
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 112;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+/***************************************** SPI6内核时钟设置 **************************************************/
+/*** FANKE ***
+>>>>> 配置说明：
+	*
+	*	1.	SPI6 所允许的最大驱动时钟为100M，详情可以查阅：7B0数据手册 第6.3.36.3小节  SPI interface characteristics
+   *
+	*	2. W25Q64JV所允许的最高驱动频率虽然不止50MHz，但使用读取指令 0x03 所允许的最大速度只能到50M
+   *
+	*	3. 这里将 pll2_q_ck 设置为 100M 作为 SPI6 的内核时钟，然后再经过2分频得到 50M 的SCK驱动时钟
+   *
+*** FANKE ***/
+
+   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI6;
+  
+   PeriphClkInitStruct.PLL2.PLL2M = 5;       // 晶振分频系数5(注：核心板使用晶振为25M)
+   PeriphClkInitStruct.PLL2.PLL2N = 100;     // 将经过预分频后的晶振时钟进行100倍倍频，得到500M时钟
+   PeriphClkInitStruct.PLL2.PLL2P = 2;       // 这个时钟无关SPI，用户可自由配置和使用
+   PeriphClkInitStruct.PLL2.PLL2R = 2;       // 这个时钟无关SPI，用户可自由配置和使用
+   PeriphClkInitStruct.PLL2.PLL2Q = 5;       // 进行5分频，得到 100M 的 pll2_q_ck 时钟
+   PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_2;
+   PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
+   PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
+
+   PeriphClkInitStruct.Spi6ClockSelection = RCC_SPI6CLKSOURCE_PLL2;	// 将 pll2_q_ck 设置为SPI6的内核时钟
+	
+   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+   {
+      Error_Handler();
+   }  
+
+
+}
+
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
